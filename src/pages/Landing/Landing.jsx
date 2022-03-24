855dcd259c17c651bc2d26a9b16484ccd04bec23
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-03-24 14:54:58 -0400

    changed landing page title

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,8 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      {/* <h1>hello, {user ? user.name : 'friend'}</h1> */}
+      <h1>Animal Feud</h1>
     </main>
   )
 }
