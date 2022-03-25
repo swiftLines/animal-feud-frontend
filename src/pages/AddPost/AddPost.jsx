fd50156ed007d459b82d2020e9b67bddf5532ee1
Jeremy Underwood <underwood.j248@gmail.com>
2022-03-25 10:48:09 -0400

    stub up AddPost

---
+++ src/pages/AddPost/AddPost.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const AddPost = (props) => {
+  return ( 
+    <>
+      <h1>AddPost</h1>
+      <form>
+        Form
+      </form>
+    </>
+  );
+}
+ 
+export default AddPost;
