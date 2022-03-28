06d3eac823cbc0c45504b2d1c66d327ba8c66800
Darnell Dugger <darnelldugger@gmail.com>
2022-03-27 21:22:22 -0400

    installed card for post list

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,17 +1,39 @@
-import { getThemeProps } from '@mui/system'
+
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+
 
 function PostCard({post}) {
-  console.log('post', post)
   return(
-    <div className="card">
-      <p>{post.owner.name}</p>
-    <Link to="/thread">
-      <h2>{post.content}</h2>
-    </Link>
-      <footer>{post.createdAt}</footer>
-    </div>
+    <Card sx={{ width: 700, m: 1 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {post.owner.name} posted at {post.createdAt}
+        </Typography>
+        <Typography variant="h5" component="div">
+        <Link to="/thread">
+          <h2>{post.content}</h2>
+        </Link>
+        </Typography>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardContent>
+    </Card>
   )
 }
+          
+
 
 export default PostCard
